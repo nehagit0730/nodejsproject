fddd6f7eac067243bbbd32e88813f3a4260a38dc
Your Name <nehabhardwaz.gtl@gmail.com>
2023-12-27 17:20:00 +0530

    create api

--- app.js
+++ app.js
@@ -3,10 +3,17 @@
 //fs.writeFileSync('new.txt','Hello this is new text');
 
 const http = require('http');
+const info = require('./data');
+const port = 4500;
 http.createServer((req,resp)=>{      // =>(this is arrow function)
+    resp.writeHead(200, { 
+        'Content-Type': 'application/json',
+      });
     resp.write("<h1>hello iam neha</h1>");
+    resp.write(JSON.stringify(info));
     resp.end();   //using for endind processing
 }
-).listen(4500); //creat and call server name :  localhost:4500(check this for result)
-const colors= require('colors');
-console.log("package.jason".red)
+)
+.listen(port); //create and call server name :  localhost:4500(check this for result)
+//const colors= require('colors');
+//console.log("package".red)
---
+++ data.js
@@ -0,0 +1,8 @@
+const info = [
+  { Name: "Neha", email: "nehabhardwaz.gtl@gmail.com", age: 32 },
+  { Name: "Puneet", email: "puneet.gtl@gmail.com", age: 30 },
+  { Name: "Pankaj", email: "pankaj.gtl@gmail.com", age: 36 },
+  { Name: "sangeeta", email: "nsangeeta.gtl@gmail.com", age: 32 },
+  { Name: "Rahul", email: "rahul.gtl@gmail.com", age: 35 },
+];
+module.exports = info;
