89efb1ecc75c346a009dfdcb4cb78268ca5e1694
Your Name <nehabhardwaz.gtl@gmail.com>
2023-12-27 14:09:10 +0530

    install packages

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 //fs.writeFileSync('new.txt','Hello this is new text');
 
 const http = require('http');
-http.createServer((req,resp)=>{
-    resp.write("hello iam neha");
+http.createServer((req,resp)=>{      // =>(this is arrow function)
+    resp.write("<h1>hello iam neha</h1>");
     resp.end();   //using for endind processing
 }
 ).listen(4500); //creat and call server name :  localhost:4500(check this for result)
+const colors= require('colors');
+console.log("package.jason".red)
