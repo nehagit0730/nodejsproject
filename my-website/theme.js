fbaa05e503fd84ffdd846bdbb9afbaba8e0867e8
Your Name <nehabhardwaz.gtl@gmail.com>
2023-12-30 13:54:01 +0530

    install ejs npm and using of ejs(call data dynamicall)

--- my-website/theme.js
+++ my-website/theme.js
@@ -6,20 +6,33 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname,'public');
 
+app.set('view engine', 'ejs')
+
 //app.use(express.static(publicPath));
 app.get('',(req,res)=> {
 res.sendFile(`${publicPath}/index.html`)
-})
+});
+
+app.get('/profile',(req,res)=> {
+    const user = {
+        name: 'Neha',
+        email: 'neha@gmail.com',
+        city: 'Dharamshala'
+    }
+    res.render('profile',{user});
+    });
 
 app.get('/contact',(req,res)=> {
     res.sendFile(`${publicPath}/contact.html`)
-    })
+    });
+
 app.get('/about',(req,res)=> {
         res.sendFile(`${publicPath}/about.html`)
-    })
+    });
 
 app.get('*',(req,res)=> {
             res.sendFile(`${publicPath}/nopage.html`)
-    })
+    });
+
 app.listen(4500);
 
