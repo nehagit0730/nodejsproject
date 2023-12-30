6677ea082e0c4dd6b8c042b981148ade1d10108a
Your Name <nehabhardwaz.gtl@gmail.com>
2023-12-30 16:54:21 +0530

    grop level middlewear

--- filesFolder/middlewear-eg.js
+++ filesFolder/middlewear-eg.js
@@ -1,5 +1,6 @@
 //APPLICATION LEVEL MIDDLEWARE USE
 //USE IN ALL APLLICATION/PAGES/ROUTE
+//APPLY GLOBALY
 /*
 const port = 5000;
 
@@ -41,7 +42,7 @@ app.listen(port);
 
 //ROUTE LEVEL MIDDLEWARE 
 //USE IN SPECIFIC PAGE/APLLICATION
-
+/*
 const port = 5000;
 
 const express= require('express');
@@ -76,5 +77,73 @@ app.get('/about',(req,res)=>{
     res.send('about page');
 });
 
+
+app.listen(port);
+*/
+
+//GROUP ROUTE MIDDLEWARE
+//if we ha 100 or more then route and we want only in 50 
+// APLLY SINGLE OR  GROUP OF ROUTER
+
+const port = 5000;
+
+const express= require('express');
+const app = express();
+const route = express.Router();
+
+
+const reqFilter = (req,res,next)=> {
+    if(!req.query.age)
+    {
+        res.send('Please enter age');
+    }
+    else if(req.query.age<18)  //if we add age less then 18 it shows please enter age"/?age=8" 
+    {
+        res.send('Please enter age');
+    }
+    else
+    {
+        next();
+    }
+}
+
+route.use(reqFilter);
+app.get('',(req,res)=>{
+    res.send('welcome');
+});
+
+app.get('/user', (req,res)=>{
+    res.send('user page');
+});
+
+app.get('/about',(req,res)=>{
+    res.send('about page');
+});
+
+route.get('/contact',(req,res)=>{
+    res.send('contact page');
+});
+
+route.get('/faq',(req,res)=>{
+    res.send('faq page');
+});
+
+route.get('/team',(req,res)=>{
+    res.send('team page');
+});
+
+app.get('/product',(req,res)=>{
+    res.send('product page');
+});
+
+app.get('/shop',(req,res)=>{
+    res.send('shop page');
+});
+
+app.get('/blog',(req,res)=>{
+    res.send('blog page');
+});
+
+app.use('/',route);
 
 app.listen(port);
