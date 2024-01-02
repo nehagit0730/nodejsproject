2dc24e386ecc2602ae4eb5d2d8404022f99b74ea
Your Name <nehabhardwaz.gtl@gmail.com>
2024-01-02 14:49:09 +0530

    create api in mongo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //const fs = require('fs');
 //fs.writeFileSync('new.txt','Hello this is new text');
 
+/*
 const http = require('http');
 const info = require('./data');
 const port = 4500;
@@ -15,5 +16,6 @@ http.createServer((req,resp)=>{      // =>(this is arrow function)
 }
 )
 .listen(port); //create and call server name :  localhost:4500(check this for result)
+*/
 //const colors= require('colors');
 //console.log("package".red)
---
+++ mongodb/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dbConnect = require('./mongodb');
+const app = express();
+app.get('/', async (req,res)=>{
+    let data = await dbConnect();
+    data = await data.find().toArray();
+        res.send(data);
+})
+
+app.listen(3000,()=>{
+    console.log('server is running');
+})
