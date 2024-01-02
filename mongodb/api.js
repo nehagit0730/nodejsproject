58c05f429f064fee1b182acfcbc4b979b77c23b4
Your Name <nehabhardwaz.gtl@gmail.com>
2024-01-02 16:58:57 +0530

    make get and post method with postman

--- mongodb/api.js
+++ mongodb/api.js
@@ -1,3 +1,5 @@
+/*
+//API GET METHOD
 const express = require('express');
 const dbConnect = require('./mongodb');
 const app = express();
@@ -10,3 +12,28 @@ app.get('/', async (req,res)=>{
 app.listen(3000,()=>{
     console.log('server is running');
 })
+*/
+
+//API POST METHOD
+
+const express = require('express');
+const dbConnect = require('./mongodb');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req,res)=>{
+    let data = await dbConnect();
+    data = await data.find().toArray();
+        res.send(data);
+});
+
+app.post('/', async (req,res)=>{
+    let data= await dbConnect();
+    let result = await data.insertOne(req.body);
+    
+        res.send(result)
+})
+
+
+app.listen(3000)
