ba09d0b718548e262dd46973a4948a6caa2f6985
Your Name <nehabhardwaz.gtl@gmail.com>
2024-01-03 12:13:48 +0530

    CRUD in mongoose

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,15 +1,52 @@
-const mongoose = require('mongoose');
 
-const main = async ()=>{
-    await mongoose.connect("mongodb://localhost:27017/ecom");
-    const productSchema = new mongoose.Schema ({
+		
+		
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/ecom");
+const productSchema = new mongoose.Schema({
         name: String,
-        price:Number
-    });
-    const ProductsModel = mongoose.model('Product', productSchema);
-    let data = new ProductsModel({name:"m8",price:430});
-    let result = await data.save();
-    console.log(result);
-}
+        price:Number,
+        category: String,
+        image: String,
+        description: String,
+        stock: Number
+});
 
-main();
+const saveInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = new Product({
+    name:"m8",
+    price:430,
+    category:"mobile",
+    image:"https://cdn.pixabay.com/photo/2017/09/08/10/00/camera-1920449_960_720.jpg",
+    description:"front camera:48megapixel.back camera:32megapixel",
+    stock:100
+  });
+  const result = await data.save();
+  console.log(result);
+};
+saveInDB();
+const updateInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = await Product.updateOne(
+    { name: "m8" },
+    {
+      $set: { price: 550, name: "max pro 6" },
+    }
+  );
+  console.log(data);
+};
+// updateInDB();
+const deleteInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = await Product.deleteOne({ name: "Glass 7" });
+  console.log(data);
+};
+const findInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = await Product.find({ name: "Glass 3" });
+  console.log(data);
+};
+// deleteInDB();
+// findInDB();
